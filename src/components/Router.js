793425c6dd98ab2d7e0743fe5a940375276a10a6
niroshini <niroshini06@gmail.com>
2021-06-13 17:51:54 +0530

    configured routes

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+import Signup from "./Signup";
 
 function Router() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Router path="/login">
+        <Login />
+      </Router>
+      <Router path="/dashboard">
+        <Dashboard />
+      </Router>
+      <Router path="/signup">
+        <Signup />
+      </Router>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
