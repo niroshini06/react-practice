5115627eff7a5a19a05d884946e206a000e82a4e
niroshini <niroshini06@gmail.com>
2021-06-16 23:30:07 +0530

    ant row,col for Chevron

--- src/atoms/Tab.js
+++ src/atoms/Tab.js
@@ -2,14 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledTab = styled.div`
-  border: solid 1px black;
   background-color: grey;
   color: #fff;
-  width: 10%;
+  width: 100px;
   height: 40px;
   line-height: 40px;
   border-radius: 10px;
   text-align: center;
+  margin: 5px;
 `;
 
 function Tab(prop) {
--- src/pages/TabTemp.js
+++ src/pages/TabTemp.js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Tab from "../atoms/Tab";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { Row, Col } from "antd";
+
+const StyledTabsDiv = styled.div`
+  border-bottom: solid 1px black;
+  height: 50px;
+  line-height: 50px;
+`;
 
 function TabTemp() {
+  const [tabCount, setTabCount] = useState([{ title: "tab1", value: "tab1" }]);
+  console.log("type check", Array.isArray(tabCount));
+
+  function addTab() {
+    let number = tabCount.length + 1;
+    let temp = {
+      title: "tab" + number,
+      value: "tab" + number,
+    };
+    setTabCount([...tabCount, temp]);
+  }
+
   return (
-    <Tab>
-      <span>hi</span>
-    </Tab>
+    <div className="outer" style={{ border: "solid 1px black" }}>
+      <StyledTabsDiv>
+        <Row align="middle">
+          <Col>
+            <FontAwesomeIcon icon={faAngleLeft} size="lg" />
+          </Col>
+          <Col>
+            <Row>
+              {tabCount.map((e, i) => (
+                <Col key={i}>
+                  <Tab>
+                    <span>{e.title}</span>
+                  </Tab>
+                </Col>
+              ))}
+            </Row>
+          </Col>
+          <Col>
+            <FontAwesomeIcon icon={faPlus} size="lg" onClick={addTab} />
+          </Col>
+        </Row>
+      </StyledTabsDiv>
+      <p>content</p>
+    </div>
   );
 }
 
